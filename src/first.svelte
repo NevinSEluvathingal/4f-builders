96d5df93b03d6d5a65e4bdc32bfd33a126e29be3
Nevin <nevinsajie@gmail.com>
2024-07-25 08:35:07 +0530

    sub section of the homepage

---
+++ src/first.svelte
@@ -0,0 +1,121 @@
+<div class="image-container">
+	<div class="image-left">
+		<img src="bg2.jpg" alt="" width="450" height="350" />
+		<p>Residential Constructions</p>
+	</div>
+	<div class="image-right">
+		<img src="bg3.jpg" alt="" width="450" height="350" />
+		<p>Commercial Constructions</p>
+	</div>
+
+	<div class="image-right">
+		<img src="bg4.jpeg" alt="" width="450" height="350" />
+		<p>Industrial Constructions</p>
+	</div>
+</div>
+
+<div class="text">
+	<h2 class="ff">About 4F Builders & Engineering Private Limited</h2>
+	<p>
+		4F Builders & Engineering Private Limited, incorporated on October 9, 2020, is a distinguished
+		player in the construction and engineering services sector. As a privately-held company
+		operating independently, it adheres to regulatory standards and maintains accountability within
+		the legal framework. Registered with the Registrar of Companies in Ernakulam, the company
+		demonstrates a commitment to compliance.
+		<br /><br /> With an authorized share capital of Rs. 100,000 and a paid-up capital of Rs.
+		100,000, 4F Builders & Engineering Private Limited specializes in complete construction projects
+		and civil engineering activities. Its most recent Annual General Meeting (AGM) date is not
+		available, and its latest balance sheet was filed on March 31, 2022, according to Ministry of
+		Corporate Affairs (MCA) records.
+		<br /><br />
+		The company's board of directors, consisting of Aji Somarajan and Prasanth Bhanu, provides strategic
+		direction and leadership to drive its objectives and growth agenda.<br /><br />
+	</p>
+</div>
+<div class="gg">
+	<h3 class="hg">Our Details</h3>
+	<ul class="lsty">
+		<li>Corporate Identification Number:(CIN) U45202KL2020PTC064932</li>
+		<li>Registration Number: 64932</li>
+		<li>Email Address: 4fbuildersengineering@gmail.com</li>
+		<li>
+			Address: X/231, Ambadiyil, Poozhikhadu, Kudassanadu P.O. Pandalam ,Pathanamthitta, 689512
+		</li>
+	</ul>
+</div>
+
+<style>
+	.hg{font-size: 50px;
+		margin-left: 45px;
+		padding-bottom: 10px;
+}
+	.gg {
+		margin-left: 45px;
+		font-size: 35px;
+		margin-top: 20px;
+		padding-top: 10px;
+	}
+
+	.ff {
+		text-align: center;
+	}
+	p {
+		text-align: center;
+	}
+	.text {
+		background-color: white;
+		margin-bottom: 0px;
+		font-size: 20px;
+		margin: 0 auto -20px;
+		font-family: poppins;
+	}
+
+	h3 {
+		font-size: 25px;
+		padding-top: 10px;
+		color: rgb(255, 255, 255);
+		margin-left: 50px;
+	}
+
+	.image-left,
+	.image-right {
+		text-align: center;
+		font-size: 20px;
+		color: white;
+		font-weight: bold;
+
+		font-family: Comfortaa;
+	}
+	.image-container {
+		margin: 0px;
+		padding: 0px;
+		margin-bottom: 0px;
+
+		padding-top: 50px;
+		background-color: black;
+		display: flex;
+		justify-content: space-around;
+	}
+
+
+
+	h2 {
+		color: rgb(0, 0, 0);
+
+		font-size: 30px;
+		padding: 10px;
+		margin-top: 0;
+		font-family: POPPINS;
+	}
+	li {
+		padding-left: 10;
+		font-family: POPPINS;
+		color: white;
+		font-size: 20px;
+		margin-bottom: 15px;
+		margin-left: 40px;
+	}
+	p {
+		padding-left: 10px;
+	}
+</style>
